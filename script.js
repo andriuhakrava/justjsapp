b686765802b1dd111156d4601c86c84fad5cbd21
andriuhakrava <andriuhakrava@gmail.com>
2021-10-07 09:36:18 +0300

    optimize fetching method and styles for it

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-const xhr = new XMLHttpRequest();
-xhr.open('GET', './data/characters.json');
-xhr.responseType = 'json';
+let characters;
+const tableView = 'TABLEVIEW';
+const preView = 'PREVIEW';
 
-xhr.onload = function() {
-    if (xhr.status === 200) {
-        let characters = xhr.response;
-        console.log('Characters:', characters);
-        console.log('Characters parsed:', JSON.parse(characters));
-    }
+function fetchCharacters() {
+    fetch('./data/characters.json')
+        .then(response => response.json())
+        .then(data => characters = data);
 }
 
-xhr.send();
+function displayCharacters(characters, viewType) {
+    const characterList = document.querySelector('.character__list');
+    characterList.classList.add(`character__list--${viewType === tableView ? 'table' : 'preview'}`);
+}
+
+document.addEventListener('DOMContentLoaded', fetchCharacters);
+
+displayCharacters(characters, tableView);
